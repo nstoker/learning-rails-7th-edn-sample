dddf971c2cbc652fc67a1ba6057a8519043cfc33
Neil Stoker <neil.stoker@gmail.com>
2022-04-24 14:29:04 +0100

    6.3.3 add hashed password to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   validates :name, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,8 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example User', email: 'user@example.com')
+    @user = User.new(name: 'Example User', email: 'user@example.com',
+                     password: 'changeme', password_confirmation: 'changeme')
   end
 
   test 'should be valid' do
@@ -18,6 +19,16 @@ class UserTest < ActiveSupport::TestCase
 
   test 'email should not be too long' do
     @user.email = 'a' * 244 + '@example.com'
+    assert_not @user.valid?
+  end
+
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
 
@@ -40,21 +51,7 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test 'email validation should reject invalid addresses' do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example foo@bar_baz.com foo@bar+baz.com foo@bar..com]
-    invalid_addresses.each do |invalid_address|
-      @user.email = invalid_address
-      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
-    end
-  end
-
-  test 'email addresses should be unique' do
-    duplicate_user = @user.dup
-    @user.save
-    assert_not duplicate_user.valid?
-  end
-
-  test 'emails are downcased on save' do
+  test 'mixed case emails are converted to lower case' do
     mixed_case_email = "UsEr@TeSt.CoM"
     @user.email = mixed_case_email
     @user.save
