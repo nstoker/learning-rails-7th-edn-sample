f5e9062fef1eafa571a114beba42669ec15ce40c
Neil Stoker <neil.stoker@gmail.com>
2022-04-24 14:14:11 +0100

    improve email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
-# frozen_string_lieral: true
-
+# frozen_string_literal: true
 
 # The user model
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  before_save { email.downcase! }
+
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
   validates :name, presence: true, length: { maximum: 50 }
 end
