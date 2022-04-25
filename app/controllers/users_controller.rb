0d8cd031219e70bb6b859aa6ce9bbb11b6f6b3e5
Neil Stoker <neil.stoker@gmail.com>
2022-04-25 22:17:46 +0100

    7.4.2 the flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
