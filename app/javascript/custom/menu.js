2d07c133dd0b56dcd25fae86e862b7125855a660
Neil Stoker <neil.stoker@gmail.com>
2022-04-30 12:37:56 +0100

    make app more mobile friendly

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -2,6 +2,13 @@
 
 // Add toggle listeners to listen for clicks
 document.addEventListener("turbo:load", function(){
+  let hamburger = document.querySelector("#hamburger");
+  hamburger.addEventListener("touchstart", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    let menu = document.querySelector("#navbar-menu");
+    menu.classList.toggle("collapse");
+  });
   let account = document.querySelector("#account");
   account.addEventListener("click", function(event) {
     event.preventDefault();
