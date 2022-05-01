79dd176b235bd696694da110ebae061298669712
Neil Stoker <neil.stoker@gmail.com>
2022-05-01 16:19:22 +0100

    8.2.7 exercises completed

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -7,10 +7,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:neil)
   end
 
-  test 'login with invalid information' do
+  test 'login with valid email/invalid password' do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: '', password: '' } }
+    post login_path, params: { session: { email: '@user.email', password: 'invalud' } }
+
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert_not flash.empty?
