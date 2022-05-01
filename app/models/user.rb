8e1333b631b6081ae66d7880d08a51ff75cf52c1
Neil Stoker <neil.stoker@gmail.com>
2022-05-01 19:02:33 +0100

    9.1.3 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,20 +15,31 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  # Returns the hash digest of the given_string
-  def self.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
-    BCrypt::Password.create string, cost:
-  end
+  class << self
+    # Returns the hash digest of the given_string
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create string, cost:
+    end
 
-  # Returns a random token
-  def self.new_token
-    SecureRandom.urlsafe_base64
+    # Returns a random token
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 
   # Remember a user in the database for use in persistent sessions
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_token, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
