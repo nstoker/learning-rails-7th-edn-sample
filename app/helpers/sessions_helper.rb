50f131e6fcd0b4a0b2a48c1df99066cf061f1e08
Neil Stoker <neil.stoker@gmail.com>
2022-05-01 17:03:20 +0100

    Chapter 8 finished

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,4 +18,10 @@ module SessionsHelper
       @current_user ||= User.find_by(id: session[:user_id])
     end
   end
+
+  # Logs out the current user
+  def log_out
+    reset_session
+    @current_user = nil
+  end
 end
