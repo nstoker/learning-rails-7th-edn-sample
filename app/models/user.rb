a1d0887f59a53c869d8ff23dbd0f370afb138152
Neil Stoker <neil.stoker@gmail.com>
2022-05-21 11:42:15 +0100

    9.1.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 # The user model
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   before_save { email.downcase! }
@@ -13,8 +15,20 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  # Returns the has digest of the given string
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create string, cost: cost
+  end
+
+  # Returns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a token in the database for use in a persistent session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
