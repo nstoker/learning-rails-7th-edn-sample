9173f4bb634249a385aadfe36cca278b7c669fd0
Neil Stoker <neil.stoker@gmail.com>
2022-06-09 19:55:15 +0100

    ch10.2.2 green

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@
 # .nodoc
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
 
   def show
     @user = User.find(params[:id])
@@ -51,5 +52,11 @@ class UsersController < ApplicationController
       flash[:danger] = 'Please log in'
       redirect_to login_url, status: :see_other
     end
+  end
+
+  # Confirms the correct user
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,8 +26,8 @@ module SessionsHelper
   # Returns the current logged-in user (if any)
   def current_user
     if (user_id = session[:user_id])
-      user = User.find_by(id: session[:user_id])
-      if user && session[:session_token] == user.remember_digest
+      user = User.find_by(id: user_id)
+      if user && session[:session_token] == user.session_token
         @current_user = user
       end
     elsif (user_id = cookies.encrypted[:user_id])
@@ -37,6 +37,10 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  def current_user?(user)
+    user && user == current_user
   end
 
   # Forgets a persistent session
