22748b7b1a4d94d70520e89be0f3c149a1aa6c26
Neil Stoker <neil.stoker@gmail.com>
2022-08-14 20:17:28 +0100

    correct typo and re-add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'earning-rails-7th-edn-sample.herokuapp.com'
-  config.action_mailer.default_url_options = { host: }
+  host = 'learning-rails-7th-edn-sample.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host}
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: '587',
